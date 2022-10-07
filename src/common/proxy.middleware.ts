57600c421d30dd811ade04929add642af6e4a23a
ss <coffee.or.tea88@gmail.com>
2022-10-07 22:46:29 +0800

    check is prod

--- src/common/proxy.middleware.ts
+++ src/common/proxy.middleware.ts
@@ -21,9 +21,13 @@ export class ProxyMiddleware implements NestMiddleware {
   ): Promise<void> {
     const ip = requestIp.getClientIp(request);
     console.log('LS -> src/common/proxy.middleware.ts:21 -> ip: ', ip);
+
     const proxyIP = this.configService.get<string>('PROXY_IP');
 
-    if (!['::ffff:127.0.0.1', proxyIP].includes(ip)) {
+    const APP_ENV = this.configService.get<string>('APP_ENV');
+    const isProd = APP_ENV !== 'dev';
+
+    if (isProd && !['::ffff:127.0.0.1', proxyIP].includes(ip)) {
       throw new UnauthorizedException('IP Access Denied');
     }
     next();
