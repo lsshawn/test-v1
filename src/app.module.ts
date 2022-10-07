d8544210949f2de26b8e87c54b0a72095338e144
Milad Ezzat <miladezzat.f@gmail.com>
2022-10-07 14:04:01 +0200

    Feature/add middlewares (#1)
    * install deps
    
    * add config
    
    * feat: add proxy middleware
    
    * feat: add user middleware
    
    * feat: using middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './common/config/configuration';
+import { ProxyMiddleware } from './common/proxy.middleware';
+import { UserMiddleware } from './common/user.middleware';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [configuration] })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ProxyMiddleware).forRoutes('*');
+    consumer.apply(UserMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  await app.listen(configService.get<number>('port'), () => {
+    console.log(
+      `Server is running on port: ${configService.get<number>('port')}`,
+    );
+  });
 }
 bootstrap();
